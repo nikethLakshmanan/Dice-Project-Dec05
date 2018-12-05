77829b1cef66dc9543cd1fed62bd942587392955
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-12-05 09:22:02 -0500

    completed test 1
    completed and successfully passed test 1

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,4 +1,8 @@
 public class DiceRunner{
 	public static void main(String[]args){
+		Dice die = new Dice();
+		Dice die2 = new Dice();
+		System.out.println(die);
+		System.out.println(die2);
 	}
 }
