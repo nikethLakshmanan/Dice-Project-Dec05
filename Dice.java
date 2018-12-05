15bd89443affd4fd484f6d044c53c658d9793e68
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-12-05 09:06:05 -0500

    Created constructors
    Created a default and nondefault constructor

--- Dice.java
+++ Dice.java
@@ -1,2 +1,14 @@
 public class Dice{
+	private int numSides;
+	private int value;
+	public Dice(int numSides){
+		this.numSides = numSides;
+		int random = (int)(Math.random()*numSides)+1;
+		value = random;
+	}
+	public Dice(){
+		numSides = 6;
+		int random2 = (int)(Math.random()*6)+1;
+		value = random2;
+	}
 }
