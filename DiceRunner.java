1849b783393939afd339fbde659117a24a0f0e46
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-12-07 09:31:03 -0500

    Added a counter
    added a counter to better the user interface

--- DiceRunner.java
+++ DiceRunner.java
@@ -2,10 +2,12 @@ public class DiceRunner{
 	public static void main(String[]args){
 		//test one
 		int numSnakeEyes = 1;
+		int count  = 1;
 		boolean counter = false;
 		while(counter==false){
 			Dice die = new Dice();
 			Dice die2 = new Dice();
+			System.out.println("Roll Number " + count);
 			System.out.println(die);
 			System.out.println(die2);
 			System.out.println();
@@ -17,6 +19,7 @@ public class DiceRunner{
 			else{
 				numSnakeEyes +=1;
 				counter = false;
+				count ++;
 			}
 		}
 	}
