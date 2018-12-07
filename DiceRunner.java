d6fa78905c1eb986de5f626baed4ae6c7e8efcfd
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-12-07 09:27:56 -0500

    Changed test1
    test1 is now fully completed

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,9 +1,23 @@
 public class DiceRunner{
 	public static void main(String[]args){
 		//test one
-		Dice die = new Dice();
-		Dice die2 = new Dice();
-		System.out.println(die);
-		System.out.println(die2);
+		int numSnakeEyes = 1;
+		boolean counter = false;
+		while(counter==false){
+			Dice die = new Dice();
+			Dice die2 = new Dice();
+			System.out.println(die);
+			System.out.println(die2);
+			System.out.println();
+			if(die.getValue() == 1 && die2.getValue() == 1){
+				System.out.println("The number of rolls needed for Snake Eyes: " + numSnakeEyes);
+				counter = true;
+
+			}
+			else{
+				numSnakeEyes +=1;
+				counter = false;
+			}
+		}
 	}
 }
